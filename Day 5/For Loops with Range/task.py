befec77d155f090f04f87f1f89fb33c1b44215eb
Boyd Querubin <boydisaacq@gmail.com>
2024-08-30 23:31:51 -0600

    glass milk juice

--- Day 5/For Loops with Range/task.py
+++ Day 5/For Loops with Range/task.py
@@ -1,5 +1,5 @@
-for number in range(1, 10, 3):
-    print(number)
+# for number in range(1, 10, 3):
+#     print(number)
 
 # var = 0
 # for number in range(1, 101):
@@ -7,14 +7,22 @@ for number in range(1, 10, 3):
 #
 # print(var)
 #
-# for number in range(1, 101):
-#     if number % 3 == 0 and number % 5 == 0:
-#         print("FizzBuzz")
-#     elif number % 5 == 0:
-#         print("Buzz")
-#     elif number % 3 == 0:
-#         print("Fizz")
-#     else:
-#         print(number)
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 5 == 0:
+        print("Buzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    else:
+        print(number)
 
 
+# glass1 = "milk"
+# glass2 = "juice"
+# glass3 = glass1
+# glass1 = glass2
+# glass2 = glass3
+#
+# print(glass1)
+# print(glass2)
