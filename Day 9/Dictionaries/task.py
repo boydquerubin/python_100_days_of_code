be694e08d5856056e0afd072360ada1bb75761dc
Boyd Querubin <boydisaacq@gmail.com>
2024-09-05 09:30:53 -0600

    dictionaries

--- Day 9/Dictionaries/task.py
+++ Day 9/Dictionaries/task.py
@@ -18,3 +18,25 @@ programming_dictionary["Bug"] = "A moth in your computer."
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
+
+    student_scores = {
+        'Harry': 88,
+        'Ron': 78,
+        'Hermione': 95,
+        'Draco': 75,
+        'Neville': 60
+    }
+
+    student_grades = {}
+
+    for score in student_scores:
+        if student_scores[score] in range(91, 100):
+            student_grades[score] = "Outstanding"
+        elif student_scores[score] in range(81, 90):
+            student_grades[score] = "Exceeds Expectations"
+        elif student_scores[score] in range(71, 80):
+            student_grades[score] = "Acceptable"
+        else:
+            student_grades[score] = "Fail"
+
+    print(student_grades)
