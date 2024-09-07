bd141933e46e8fbc9371bc8e875fc476fdd3cab3
Boyd Querubin <boydisaacq@gmail.com>
2024-09-07 11:59:22 -0600

    part 4 calculator project first attempt

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,40 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+first_number = int(input("Type your first number "))
+math_op = input("Choose your mathematical operator (a choice of '+', '-', '*' or '/') ")
+second_number = int(input("Type your second number "))
+
+# print(operations["*"](4,8))
+
+# while True:
+#     if math_op == "+":
+#         (operations["*"](first_number, second_number)
+#     if math_op == "-":
+#         (operations["*"](first_number, second_number)
+#     if math_op == "*":
+#         (operations["*"](first_number, second_number)
+#     if math_op == "/":
+#         (operations["*"](first_number, second_number)
+
+# operations["*"]
+
+# for operate in operations:
+#     if
