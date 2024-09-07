f553684afe569803ff87e53d6dcead8b8c9bda51
Boyd Querubin <boydisaacq@gmail.com>
2024-09-06 20:34:11 -0600

    review

--- Day 9/Blind Auction Project/main.py
+++ Day 9/Blind Auction Project/main.py
@@ -24,3 +24,13 @@ highest_bidder = max(bids, key=bids.get)
 highest_bid = bids[highest_bidder]
 
 print(f"The winner is: {highest_bidder}, with the highest bid: {highest_bid}")
+
+# def find_highest_bidder(bidding_dictionary):
+#     highest_bid = 0
+#     for bidder in bidding_dictionary:
+#         bid_amount = bidding_dictionary(bidder)
+#         if bid_amount > highest_bid:
+#             highest_bid = bid_amount
+#             winner = bidder
+#
+#     print(f"The winner is {winner} with a bid of ${highest_bid}")
