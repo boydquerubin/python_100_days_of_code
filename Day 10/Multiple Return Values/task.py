a251b9ffc6452ed23bf90de419003bda42ead2eb
Boyd Querubin <boydisaacq@gmail.com>
2024-09-07 10:31:45 -0600

    leapyear

--- Day 10/Multiple Return Values/task.py
+++ Day 10/Multiple Return Values/task.py
@@ -9,3 +9,18 @@ def format_name(f_name, l_name):
 print(format_name("AnGEla", "YU"))
 
 print(format_name(input("What is your first name? "), input("What is your last name? ")))
+
+# def is_leap_year(year):
+#     if year % 400 == 0:
+#         return True
+#     if year % 100 == 0:
+#         return False
+#     if year % 4 == 0:
+#         return True
+#     return False
+#
+# print(is_leap_year(2000))
+# print(is_leap_year(1900))
+# print(is_leap_year(2024))
+# print(is_leap_year(2023))
+# print(is_leap_year(1989))
