6bf4c7c94428dff634796c62089c3d315ebc0454
Boyd Querubin <boydisaacq@gmail.com>
2024-09-10 09:08:12 -0600

    making comments

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -9,28 +9,25 @@ current_score = 0
 
 if game_start == "y":
     print(f"\n"*20)
+
     import art
     print(art.logo)
+
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
     first_round = random.choices(cards, k=2)
-    for _ in first_round:
-        score = sum(first_round)
 
-    current_score = score
+    current_score = sum(first_round)
 
     print(f"Your cards: {first_round}, current score: {current_score}")
 
+    hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
+    if hit_or_stand == "y":
+        second_round = random.choices(cards, k=1)
 
-
-    # def player_cards():
-    #     for num in cards:
-    #         print(random.shuffle(cards))
-
-
-
-    game_start = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
-
+        current_score = sum(first_round + second_round)
+        print(f"Your cards: {first_round}{second_round}, current score: {current_score}")
+        #if score over 23 you lose
 
 if game_start == "n":
     print("Bye, have a wonderful day!")
