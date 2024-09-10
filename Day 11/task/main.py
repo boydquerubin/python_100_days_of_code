d469c85c61dda8ebdaa53cacacd09619f712f6d8
Boyd Querubin <boydisaacq@gmail.com>
2024-09-10 07:39:01 -0600

    blackjack

---
+++ Day 11/task/main.py
@@ -0,0 +1,9 @@
+#create a flowchart
+#try solving without hints
+
+input("") #function inertion or inception lol
+
+import art
+print(art.logo)
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
