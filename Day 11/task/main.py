4cc9e3a6f0c219173bba508725d141c98d54a427
Boyd Querubin <boydisaacq@gmail.com>
2024-09-11 09:20:12 -0600

    comments

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -6,21 +6,30 @@ import random
 game_start = input("Do you want to play Blackjack? Type 'Y' or 'N' ").lower() #function inertia? or inception lol greeting disappears\n*20
 
 current_score = 0
+dealer_current_score = 0
 
 if game_start == "y":
     print(f"\n"*20)
 
     import art
     print(art.logo)
-
+    #Computer thinks Ace is only 11
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     hand = [] #empty array to show hand?
+    dealer_hand = []
 
     first_round = random.choices(cards, k=2)
     hand.extend(first_round)
     current_score = sum(hand)
 
-    print(f"Your cards: {first_round}, current score: {current_score}")
+    print(f"Your cards: {first_round}, your current score: {current_score}")
+    #computer's hand
+    dealer_first_round = random.choices(cards, k=2)
+    dealer_hand.extend(dealer_first_round)
+    dealer_current_score = sum(dealer_hand)
+    print(f"Dealer's hand: {dealer_hand}, Dealer's current score: {dealer_current_score}")
+
+
     if current_score <= 21:
         hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
 
@@ -29,7 +38,7 @@ if game_start == "y":
             print(f"The dealer deals you a {second_round}.")
             hand.extend(second_round)
             current_score = sum(hand)
-            print(f"Your cards: {hand}, current score: {current_score}")
+            print(f"Your cards: {hand}, your current score: {current_score}")
             print("Dealer's hand is: X")
 
             if current_score <= 21:
@@ -44,11 +53,11 @@ if game_start == "y":
             print("Dealer's hand is: X")
 
     elif current_score == 21:
-        print(f"Your cards: {hand}, current score: {current_score}")
+        print(f"Your cards: {hand}, your current score: {current_score}")
         print("Dealer's hand is: X")
         print(f"You hit {current_score}, You win!")
     else:
-        print(f"Your cards: {hand}, current score: {current_score}")
+        print(f"Your cards: {hand}, your current score: {current_score}")
         print("Dealer's hand is: X")
         print("You went over 21. Bust, you lose!")
 
