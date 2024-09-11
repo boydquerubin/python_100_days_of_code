9369ba44c88817ab99d7c2050f3f391241c83b43
Boyd Querubin <boydisaacq@gmail.com>
2024-09-11 08:54:51 -0600

    blackjack logic

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -17,17 +17,47 @@ if game_start == "y":
     hand = [] #empty array to show hand?
 
     first_round = random.choices(cards, k=2)
-
-    current_score = sum(first_round)
+    hand.extend(first_round)
+    current_score = sum(hand)
 
     print(f"Your cards: {first_round}, current score: {current_score}")
+    if current_score <= 21:
+        hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
 
-    hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
-    if hit_or_stand == "y":
-        second_round = random.choices(cards, k=1)
+        if hit_or_stand == "y":
+            second_round = random.choices(cards, k=1)
+            print(f"The dealer deals you a {second_round}.")
+            hand.extend(second_round)
+            current_score = sum(hand)
+            print(f"Your cards: {hand}, current score: {current_score}")
+            print("Dealer's hand is: X")
 
-        current_score = sum(first_round + second_round)
-        print(f"Your cards: {first_round}{second_round}, current score: {current_score}")
+            if current_score <= 21:
+                hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
+            elif current_score == 21:
+                print(f"You hit {current_score}, You win!")
+            else:
+                print("You went over 21. Bust, you lose!")
+
+        if hit_or_stand == "n":
+            print(f"Your score is {current_score}.")
+            print("Dealer's hand is: X")
+
+    elif current_score == 21:
+        print(f"Your cards: {hand}, current score: {current_score}")
+        print("Dealer's hand is: X")
+        print(f"You hit {current_score}, You win!")
+    else:
+        print(f"Your cards: {hand}, current score: {current_score}")
+        print("Dealer's hand is: X")
+        print("You went over 21. Bust, you lose!")
+
+    if hit_or_stand == "n":
+        print(f"Your score is {current_score}.")
+        print("Dealer's hand is: X")
+
+
+
         #if score over 23 you lose
 
 if game_start == "n":
