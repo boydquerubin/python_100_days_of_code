60295687b62f969fe555afad047453346e211eae
Boyd Querubin <boydisaacq@gmail.com>
2024-09-11 22:49:42 -0600

    dealer hand

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -39,10 +39,15 @@ if game_start == "y":
             hand.extend(second_round)
             current_score = sum(hand)
             print(f"Your cards: {hand}, your current score: {current_score}")
-            print("Dealer's hand is: X")
+
+            dealer_second_round = random.choices(cards, k=1)
+            dealer_hand.extend(dealer_second_round)
+            dealer_current_score = sum(dealer_hand)
+            print(f"Dealer's hand: {dealer_hand}, Dealer's current score: {dealer_current_score}")
 
             if current_score <= 21:
                 hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
+                #third round?
             elif current_score == 21:
                 print(f"You hit {current_score}, You win!")
             else:
@@ -67,7 +72,7 @@ if game_start == "y":
 
 
 
-        #if score over 23 you lose
+        #if score over 21 you lose
 
 if game_start == "n":
     print("Bye, have a wonderful day!")
