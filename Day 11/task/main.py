844fd1618785beec94a98fe73a72e15a355aaa81
Boyd Querubin <boydisaacq@gmail.com>
2024-09-12 10:45:56 -0600

    more

--- Day 11/task/main.py
+++ Day 11/task/main.py
@@ -31,9 +31,35 @@ if game_start == "y":
 
     hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
 
+    # attempt to redraw for dealer
+    if dealer_current_score < 21 and dealer_current_score > 2:
+        dealer_second_round = random.choices(cards, k=1)
+        dealer_hand.extend(dealer_second_round)
+        dealer_current_score = sum(dealer_hand)
+        if dealer_current_score < 21:
+            print(f"Dealer hits again. Dealer's hand: {dealer_hand}, Dealer's current score: {dealer_current_score}")
+            if dealer_current_score < 21 and dealer_current_score > 13:
+                dealer_second_round = random.choices(cards, k=1)
+                dealer_hand.extend(dealer_second_round)
+                dealer_current_score = sum(dealer_hand)
+
+                if dealer_current_score <= 21:
+                    print(f"Dealer's hand: {dealer_hand}, Dealer's current score: {dealer_current_score}, dealer wins.")
+                else:
+                    print(f"Dealer's hand: {dealer_hand}, Dealer's current score: {dealer_current_score}, dealer bust.")
+        else:
+            print(f"Dealer hits again. Dealer's hand: {dealer_hand}, Dealer's current score: {dealer_current_score}")
+
     if hit_or_stand == "n":
-        print(f"Your score is {current_score}.")
-        print("Dealer's hand is: X")
+        if current_score > dealer_current_score:
+            print(f"Your score is {current_score}. Dealer's score is: {dealer_current_score}. You win!!!")
+        if dealer_current_score > current_score and dealer_current_score > 21:
+            print(f"Your score is {current_score}. Dealer's score is: {dealer_current_score}. Dealer Bust, you win!!!")
+        if dealer_current_score > current_score and dealer_current_score < 21:
+            print(f"Your score is {current_score}. Dealer's score is: {dealer_current_score}. Dealer wins.")
+        # compare_scores = max(current_score, dealer_current_score)
+        # if compare_scores <= 21:
+        #     print(f"Your score is {current_score}. Dealer's score is: {dealer_current_score}. {compare_scores} wins!")
 
     if current_score <= 21:
         hit_or_stand = input("Type 'Y' to Hit and 'N' to Stand\n").lower()
@@ -72,10 +98,9 @@ if game_start == "y":
         print("You went over 21. Bust, you lose!")
 
     if hit_or_stand == "n":
-        print(f"Your score is {current_score}.")
-        print("Dealer's hand is: X")
-
-
+        # if the dealer's hand is under 21, make it draw another card
+        # if it is close to 20, make it not draw any more cards
+        print(f"Your score is {current_score}. Dealer's score is: {dealer_current_score}.")
 
         #if score over 21 you lose
 
