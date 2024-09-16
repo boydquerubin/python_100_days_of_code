136909f4fce5b03caa79119207b2860d22b8415e
Boyd Querubin <boydisaacq@gmail.com>
2024-09-16 07:59:29 -0600

    local vs global scope

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -8,3 +8,25 @@ def increase_enemies():
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
+
+# Local Scope
+
+# def drink_potion():
+#     potion_strength = 2
+#     print(potion_strength)
+
+# drink_potion()
+# print(potion_strength) """undefined, potion_strength is only accessible inside the function in a local scope"""
+
+# Global Scope
+
+player_health = 10
+
+def game():
+    def drink_potion():
+        potion_strength = 2
+        print(player_health)
+
+    drink_potion()
+
+print(player_health)
